bb8a8f2e45323527a65e4bad1ccceb068327c369
hjenp22 <hjipark22@gmail.com>
2024-01-08 11:50:07 -0800

    mysql close connect

--- Assets/config/mysql.js
+++ Assets/config/mysql.js
@@ -20,10 +20,20 @@ const db = mysql.createConnection(
     console.log('Connected to the company_db database');
   });
 
+  const addEmployee = (employee) => {
+    const { first_name, last_name, role_id, manager_id } = employee;
+  
+    db.query(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);",
+      [first_name, last_name, role_id, manager_id || null],
+      (err, result) => {
+        if (err) {
+          console.error('Error adding employee to the database:', err);
+        } else {
+          console.log("Employee added successfully!");
+        }
+      }
+    );
+  };
 
-
-
-
-
-
-module.exports = { db, addEmployee };
+module.exports = { db, addEmployee, closeConnection: () => db.end() };
