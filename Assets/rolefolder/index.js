0b3119f4a7d4bcfb2a1155474c421fd487d315f1
hjenp22 <hjipark22@gmail.com>
2024-01-09 15:40:36 -0800

    roles index.js slay

---
+++ Assets/rolefolder/index.js
@@ -0,0 +1,45 @@
+const inquirer = require("inquirer");
+const {addRole, viewRoles} = require("./Assets/config.role");
+
+const db = require("./config/mysql")
+
+const mainMenu = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "menuChoice",
+                message: "Select an option:",
+                choices: [
+                    "Add a new role",
+                    "View all roles",
+                    "EXIT",
+                ],
+            },
+        ])
+        .then((answers) => {
+            switch (answers.menuChoice) {
+              case "Add New Role":
+                addRole(mainMenu);
+                break;
+              case "View All Roles":
+                viewRoles(mainMenu);
+                break;
+              case "Exit":
+                console.log("Exiting the application.");
+                db.closeConnection(); // Close the database connection
+                break;
+              default:
+                console.log("Invalid choice. Please select a valid option.");
+                mainMenu();
+                break;
+            }
+          })
+          .catch((error) => {
+            console.error("Error during inquirer prompt:", error);
+          });
+      };
+      
+      // Start the main menu
+      mainMenu();
+      
